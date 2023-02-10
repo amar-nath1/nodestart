4ab0a01feb29a56ccb8cf19891e3b7eeb81cc3b6
amar-nath1 <amarat279955>
2023-02-10 20:27:18 +0530

    diff message for diff urls

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const http=require('http')
 
 let server=http.createServer((req,res)=>{
-    console.log('My name is Amar')
+    let message
+    if (req.url==='/'){
+        message='Welcome to homepage'
+    }
+    else if (req.url==='/about'){
+        message='Welcome to About page'
+    }
+    else if (req.url==='/node'){
+        message='Welcome to NodeJs project'
+    }
+    let htmlcode=`
+    <html>
+    <head>
+    <title>amar node</title>
+    <body>
+    <h1>${message}</h1>
+    </body>
+    </head>
+    </html>
+    `
+    res.write(htmlcode)
+    res.end()
 })
 
 server.listen(3000)
