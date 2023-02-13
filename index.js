e44cd730eeb3302cbbd4947b34bf80c5040e851f
amar-nath1 <amarat279955>
2023-02-13 15:54:08 +0530

    Chat App demo

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-const http=require('http')
 
-let server=http.createServer((req,res)=>{
-    let message
+const http=require('http')
+const fs=require('fs')
+
+const server=http.createServer((req,res)=>{
+
+
+    
+
     if (req.url==='/'){
-        message='Welcome to homepage'
-    }
-    else if (req.url==='/about'){
-        message='Welcome to About page'
-    }
-    else if (req.url==='/node'){
-        message='Welcome to NodeJs project'
-    }
-    let htmlcode=`
+        
+        let mydata=fs.readFileSync('message.txt','utf-8')
+        let htmlCode=`
     <html>
-    <head>
-    <title>amar node</title>
-    <body>
-    <h1>${message}</h1>
-    </body>
-    </head>
-    </html>
+
+<head></head>
+<title>Apun ka server</title>
+
+<body>
+<p>${mydata}</p>
+<form action="/message" method="post">
+    <input type="text" placeholder="enter a message" name='message'>
+    <button type="submit">Send</button>
+</form>
+</body>
+
+</html>
     `
-    res.write(htmlcode)
-    res.end()
+        res.write(htmlCode)
+        return res.end()
+    }
+
+    if (req.url==='/message' && req.method==='POST'){
+        const body=[]
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            fs.writeFileSync('message.txt', parsedBody.split('=')[1])
+        })
+        
+        res.statusCode=302
+        res.setHeader('Location','/')
+        return res.end()
+    }
+let htmlCodeforelse=`
+
+<html>
+
+<head></head>
+
+
+<body>
+<h3>Thanks for Your message</h3>
+</body>
+
+</html>
+
+
+`
+    res.write(htmlCodeforelse)
+    return res.end()
+
 })
 
 server.listen(3000)
+
